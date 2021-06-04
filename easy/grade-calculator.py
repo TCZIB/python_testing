136e1493222ddad354c81441c54680285fd32d3c
TCZIB <tcalvin00@outlook.com>
2021-06-04 14:39:40 +0000

    New

---
+++ easy/grade-calculator.py
@@ -0,0 +1,16 @@
+
+def request_inputs():
+    subjects = ["Mathmatics", "Chemistry", "Physics"]
+    subject_marks = {}
+    for subject in subjects:
+        subject_marks[subject] = int(input("What was your mark in "+subject+": "))
+
+return subject_marks
+
+def generate_average():
+    pass
+
+def generate_percentage_grade():
+    pass
+
+request_inputs()
