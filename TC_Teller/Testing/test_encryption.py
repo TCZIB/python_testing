38ef0e8d8f17d92aa501e0a14396eb7045d591de
TCZIB <tcalvin00@outlook.com>
2021-06-09 10:24:00 +0100

    Init Commit

--- TC_Teller/Testing/test_encryption.py
+++ TC_Teller/Testing/test_encryption.py
@@ -1,11 +1,21 @@
-import sys
+import random
+from ..Sub_Programs.encryption import num_scramble , num_scramble_undo, string_scrable, string_scramble_undo
 
-sys.path.append('~/python_testing/TC_Teller/Sub_Programs')
+def test_encrypt_number():
 
-from random import randint
-from encryption import num_scramble , num_scramble_undo
+    for i in range(200):
 
-def test_encrypt():
-    x = random.randint(1000,9999)
-    num = num_scramble(x)
-    assert x == number_scramble_undo(num)
+        x = random.randint(1000,9999)
+        num = num_scramble(x)
+        num2 = num_scramble_undo(num)
+        print(x)
+        print(num2)
+        assert int(x) == int(num2)
+
+def test_encrypt_string():
+
+    z = "Hello"
+    x = string_scrable(z)
+    y = string_scramble_undo(x)
+
+    assert z == y
