3de8b8a30cc840da6e34b2444239f04be8ba49de
TCZIB <tcalvin00@outlook.com>
2021-06-10 15:15:26 +0100

    Working with abstraction, no data is stored. Can check users balance

--- TC_Teller/main.py
+++ TC_Teller/main.py
@@ -7,12 +7,16 @@ from Sub_Programs.validation import verify
 
 class user:
 
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
+    def __init__(self, credentials):
+
+        self.name = string_scramble_undo(credentials[0])
+        self.balance = num_scramble_undo(credentials[2])
     
     def get_balance_user(self):
-        return self.balance
+
+        output = f"{self.name} your balance is £{self.balance}"
+
+        return output
     
     def withdraw_user(self, amount):
         self.balance = self.balance - amount
@@ -21,6 +25,15 @@ class user:
     def deposit_user(self, amount):
         return self.balance + amount
 
+    def display_options(self):
+        print("\n")
+        print(f"Welcome {self.name}")
+        print("From the list below please type in option:")
+        pass
+
+    def user_options(self):
+        pass
+
 
 def user_login():
 
@@ -28,12 +41,16 @@ def user_login():
     input_pin = input("What is your pin: ")
 
     info = [input_name, input_pin, 0]
-
     info = verify(info)
 
     return info
 
-def search_csv(name, pin):
+def search_csv(info):
+
+    name = info[0]
+    pin = info[1]
+
+    original_info = info
 
     parentDirectory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
     user_path = parentDirectory + "/TC_Teller/Usually_Hidden/Users_Data/users.csv"
@@ -42,9 +59,35 @@ def search_csv(name, pin):
 
     for row in csv_file:
         if str(name) == row[0] and str(pin) == row[1]:
-            print(row)
+            return row
     
-    print(row)
-    
+    return "User Not Found"
 
-print(user_login())
+def user_generation(info):
+    logged_in_user = user(string_scramble_undo(info[0]), num_scramble_undo(info[1]))
+    print(user.get_balance_user(logged_in_user))
+
+def main_loop():
+    print("\n")
+    credentials = user_login()
+
+    #Checks to see if list or str returned
+    if type(credentials) == str:
+        #If string returned must be error message, re-run login
+        print(credentials)
+    #If its not a string, must be list
+    else:
+        #Searches the CSV file to see if it can find the user
+        credentials = search_csv(credentials)
+        #If it returns a string, must be error
+        if type(credentials) == str:
+            #Prints error
+            print(credentials)
+        else:
+            logged_in_user = user(credentials)
+            print(logged_in_user.get_balance_user())
+
+while True:
+    main_loop()
+
+
