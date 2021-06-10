3c499d2db13c987a0ae008e1f51d49bfe646a24d
TCZIB <tcalvin00@outlook.com>
2021-06-10 15:55:07 +0100

    Worked on main class structure, ability to traverse menu, added view balance

--- TC_Teller/main.py
+++ TC_Teller/main.py
@@ -11,12 +11,29 @@ class user:
 
         self.name = string_scramble_undo(credentials[0])
         self.balance = num_scramble_undo(credentials[2])
+
+        self.display_options()
     
-    def get_balance_user(self):
+    def verify_y_n(self):
 
-        output = f"{self.name} your balance is £{self.balance}"
+        print("\n")
+        usr_input = input("Return to menu y/n: ")
 
-        return output
+        if usr_input.lower() != "y" or usr_input != "n":
+            self.verify_y_n()
+        
+        return usr_input
+
+    def view_balance(self):
+
+        print("\n")
+        print(f"{self.name} your balance is £{self.balance}")
+        usr_input = input("Return to menu y/n: ")
+    
+        if usr_input == "y":
+            self.display_options()
+        
+        return
     
     def withdraw_user(self, amount):
         self.balance = self.balance - amount
@@ -26,14 +43,48 @@ class user:
         return self.balance + amount
 
     def display_options(self):
+        options = {"1":". Exit","2":". View Balance","3":". Withdraw","4":". Deposit"}
+
         print("\n")
         print(f"Welcome {self.name}")
         print("From the list below please type in option:")
-        pass
+        for option in options:
+            print(option + options[option])
 
-    def user_options(self):
-        pass
+        usr_input = input("Selection: ")
+        
+        usr_input = self.verify_input(usr_input, options)
 
+        if usr_input:
+            pass
+        if not usr_input:
+            self.display_options
+
+        if usr_input == "Exit":
+            print("Have a nice day!")
+            print("\n")
+            return
+
+        next_func = "self."+((options[usr_input]).lower()[2:]).replace(" ","_")+"()"
+
+        exec(next_func)
+        
+        
+    
+    def verify_input(self, usr_input, options):
+
+        usr_input = usr_input.lower()
+
+        valid_input = list(str(options.keys()))
+        
+        if str(usr_input) not in valid_input:
+            print("1")
+            return False
+
+        if usr_input == str(1):
+            return "Exit"
+        
+        return usr_input
 
 def user_login():
 
@@ -85,7 +136,6 @@ def main_loop():
             print(credentials)
         else:
             logged_in_user = user(credentials)
-            print(logged_in_user.get_balance_user())
 
 while True:
     main_loop()
